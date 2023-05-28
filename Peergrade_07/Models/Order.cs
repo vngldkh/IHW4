7ddcb655d7cf288eb007c26cae79b287639ec4a2
vngldkh <idgladkikh@edu.hse.ru>
2023-05-28 20:06:26 +0300

    Comments added

--- Peergrade_07/Models/Order.cs
+++ Peergrade_07/Models/Order.cs
@@ -4,17 +4,35 @@ using System.Runtime.Serialization;
 
 namespace IHW4.Models
 {
+    /// <summary>
+    /// Класс, описывающий заказ
+    /// </summary>
     [DataContract]
     public class Order
     {
+        /// <summary>
+        /// Идентификатор пользователя (который сделал заказ)
+        /// </summary>
         [DataMember, Required]
         public Int64 UserID { get; set; }
+        /// <summary>
+        /// Статус заказа
+        /// </summary>
         [DataMember, Required]
         public String Status { get; set; }
+        /// <summary>
+        /// Особые пожелания к заказу
+        /// </summary>
         [DataMember]
         public String SpecialRequests { get; set; }
+        /// <summary>
+        /// Время создания заказа
+        /// </summary>
         [DataMember, Required]
         public DateTime CreatedAt { get; set; }
+        /// <summary>
+        /// Время обновления заказа
+        /// </summary>
         [DataMember, Required]
         public DateTime UpdatedAt { get; set; }
         
--- Peergrade_07/Models/OrderItem.cs
+++ Peergrade_07/Models/OrderItem.cs
@@ -3,12 +3,21 @@ using System.Runtime.Serialization;
 
 namespace IHW4.Models
 {
+    /// <summary>
+    /// Класс, описывающий блюда во время создания заказа
+    /// </summary>
     [DataContract]
     public class OrderItem
     {
+        /// <summary>
+        /// Название блюда
+        /// </summary>
         [DataMember, Required]
         public string Name { get; set; }
         
+        /// <summary>
+        /// Количество
+        /// </summary>
         [DataMember, Required]
         public int Quantity { get; set; }
     }
