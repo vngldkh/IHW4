14d5fa3be67456187e90fd90a8cbd874fd951cec
vngldkh <idgladkikh@edu.hse.ru>
2023-05-28 19:41:54 +0300

    Comments added

--- Peergrade_07/Models/OrderDish.cs
+++ Peergrade_07/Models/OrderDish.cs
@@ -2,13 +2,28 @@
 
 namespace IHW4.Models
 {
+    /// <summary>
+    /// Класс, содержащий информацию о блюде в заказе
+    /// </summary>
     public class OrderDish
     {
-        public Int64 OrderID { get; set; }
-        public Int64 DishID { get; set; }
-        public int Quantity { get; set; }
-        public decimal Price { get; set; }
-
+        /// <summary>
+        /// Идентификатор соответствующего заказа
+        /// </summary>
+        public Int64 OrderID { get; }
+        /// <summary>
+        /// Идентификатор соответствующего блюда
+        /// </summary>
+        public Int64 DishID { get; }
+        /// <summary>
+        /// Количество
+        /// </summary>
+        public int Quantity { get; }
+        /// <summary>
+        /// Стоимость во время заказа
+        /// </summary>
+        public decimal Price { get; }
+        
         public OrderDish(Int64 orderId, Int64 dishId, int quantity, decimal price)
             => (OrderID, DishID, Quantity, Price) = (orderId, dishId, quantity, price);
     }
